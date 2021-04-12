c21dff2a6b5a7ee34eb251fbe6b99e1697b51818
Thiago Cabral <ericson.thiago@gmail.com>
2021-04-12 10:57:35 -0300

    First commit

---
+++ src/main/java/br/com/treinamento/autenticarusuario/AutenticarUsuarioApplication.java
@@ -0,0 +1,13 @@
+package br.com.treinamento.autenticarusuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutenticarUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutenticarUsuarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/treinamento/autenticarusuario/AutenticarUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.treinamento.autenticarusuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutenticarUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
